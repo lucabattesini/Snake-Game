3d7a50b8386d24547850b75ea7269b9bd7388db3
Luca Battesini <lucabattesini1@gmail.com>
2025-10-13 13:08:15 -0300

    Creating snake class

---
+++ Characters/DefaultSnake.cs
@@ -0,0 +1,48 @@
+namespace Characters
+{
+    public class DefaultSnake
+    {
+        private int Dx = 1;
+        private int Dy = 0;
+
+        public int X
+        {
+            get;
+            private set;
+        }
+        public int Y
+        {
+            get;
+            private set;
+        }
+
+        public bool isAlive
+        {
+            get;
+            set;
+        }
+
+        public DefaultSnake(int startX, int startY)
+        {
+            isAlive = true;
+            X = startX;
+            Y = startY;
+        }
+
+        public void Move()
+        {
+            X += Dx;
+            Y += Dy;
+        }
+        public void ChangeDirection(int newDx, int newDy)
+        {
+            Dx = newDx;
+            Dy = newDy;
+        }
+        public void Collide()
+        {
+            isAlive = false;
+        }
+        
+    }
+}
